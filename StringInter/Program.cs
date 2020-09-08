e574b2476b9a391606482a93ccb81e952fbde064
Opilane TTHK <opilane@tthk.ee>
2020-09-08 11:50:21 +0300

    First commit

---
+++ StringInter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what's your first name?");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("What's your last name?");
+            string LastName;
+            LastName = Console.ReadLine();
+            string FullName;
+            FullName = Name + " " + LastName;
+            //Console.WriteLine("Hello, " + FullName);
+            Console.WriteLine($"Hello, {Name} {LastName}");
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good morning!");
+            Console.WriteLine("Hello, what's your first name?");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("What's your last name?");
+            string LastName;
+            LastName = Console.ReadLine();
+            string FullName;
+            FullName = Name + " " + LastName;
+            //Console.WriteLine("Hello, " + FullName);
+            //Console.WriteLine($"Hello, {Name} {LastName}");
+            Console.WriteLine("What's your year of birth?");
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int YearOfBirth;
+            // parsing string to interger (number)
+            YearOfBirth = Int32.Parse(UserInput);
+            int Age = 2020 - YearOfBirth;
+            Console.WriteLine($"Hello {FullName}, you are {Age} years old.");
+        }
+    }
+}
